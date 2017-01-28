61e6b56a267d0bfd07b028eaaea9f85c061e03c6
Skuznetz <s.kuznetzov@mail.ru>
2017-01-28 20:57:20 +0200

    Revert "db"
    This reverts commit 034d32faca72611b3794c406c2d960a30254fbd7.

--- db/migrate/20170128171513_create_pages.rb
+++
@@ -1,9 +0,0 @@
-class CreatePages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :pages do |t|
-      t.string :name,null: false,default: ''
-      t.string :description,null: false,default: ''
-      t.timestamps null: false
-    end
-  end
-end
