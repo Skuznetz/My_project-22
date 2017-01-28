034d32faca72611b3794c406c2d960a30254fbd7
Skuznetz <s.kuznetzov@mail.ru>
2017-01-28 19:45:46 +0200

    db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-	root 'pages#welcome'
-	get 'about', to: 'pages#about'
+	root 'static_pages#welcome'
+	get 'about', to: 'static_pages#about'
+	resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20170128171513_create_pages.rb
@@ -0,0 +1,9 @@
+class CreatePages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pages do |t|
+      t.string :name,null: false,default: ''
+      t.string :description,null: false,default: ''
+      t.timestamps null: false
+    end
+  end
+end
