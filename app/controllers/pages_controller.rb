36e7af7e951f50af592295249ac21af61d2a6309
Skuznetz <s.kuznetzov@mail.ru>
2017-01-29 21:31:58 +0200

    Revert "controller pages"
    This reverts commit dee4a56dd9780dbc5017b623e5f9d3c181d964e6.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,8 @@
 class PagesController < ApplicationController
+	def welcome
+	@title = 'Здравствуйте'
+end
+   def about
+	@title = 'Мы меняемся '
+end
 end
--- app/controllers/static_pages_controller.rb
+++
@@ -1,8 +0,0 @@
-class StaticPagesController < ApplicationController
-	def welcome
-	@title = 'Здравствуйте'
-end
-   def about
-	@title = 'Мы меняемся '
-end
-end
