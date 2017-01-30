dcc258c9cae2fe6e4e4cdf0f38e9457cf226d661
Skuznetz <s.kuznetzov@mail.ru>
2017-01-30 18:31:29 +0200

    new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,20 @@
 class PagesController < ApplicationController
+	def new
+		@page = Page.new
+	end
+
+	def create
+	end
+
+	def index
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
